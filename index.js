fd1e011377fcd49e030caab3a7c58d0b5b5bfa7c
Micah <micahronan@proton.me>
2025-05-03 15:55:56 -0500

    Completed singleplayer.html file so that server doesnt have to calculate for them

--- index.js
+++ index.js
@@ -50,59 +50,20 @@ const valueCards = (cards, deckWithValues) => {
 };
 
 const deckOfCards = {
-  "Ace of Clubs": 11,
-  "Ace of Diamonds": 11,
-  "Ace of Hearts": 11,
-  "Ace of Spades": 11,
-  "Two of Clubs": 2,
-  "Two of Diamonds": 2,
-  "Two of Hearts": 2,
-  "Two of Spades": 2,
-  "Three of Clubs": 3,
-  "Three of Diamonds": 3,
-  "Three of Hearts": 3,
-  "Three of Spades": 3,
-  "Four of Clubs": 4,
-  "Four of Diamonds": 4,
-  "Four of Hearts": 4,
-  "Four of Spades": 4,
-  "Five of Clubs": 5,
-  "Five of Diamonds": 5,
-  "Five of Hearts": 5,
-  "Five of Spades": 5,
-  "Six of Clubs": 6,
-  "Six of Diamonds": 6,
-  "Six of Hearts": 6,
-  "Six of Spades": 6,
-  "Seven of Clubs": 7,
-  "Seven of Diamonds": 7,
-  "Seven of Hearts": 7,
-  "Seven of Spades": 7,
-  "Eight of Clubs": 8,
-  "Eight of Diamonds": 8,
-  "Eight of Hearts": 8,
-  "Eight of Spades": 8,
-  "Nine of Clubs": 9,
-  "Nine of Diamonds": 9,
-  "Nine of Hearts": 9,
-  "Nine of Spades": 9,
-  "Ten of Clubs": 10,
-  "Ten of Diamonds": 10,
-  "Ten of Hearts": 10,
-  "Ten of Spades": 10,
-  "Jack of Clubs": 10,
-  "Jack of Diamonds": 10,
-  "Jack of Hearts": 10,
-  "Jack of Spades": 10,
-  "Queen of Clubs": 10,
-  "Queen of Diamonds": 10,
-  "Queen of Hearts": 10,
-  "Queen of Spades": 10,
-  "King of Clubs": 10,
-  "King of Diamonds": 10,
-  "King of Hearts": 10,
-  "King of Spades": 10,
-};
+  "A♣": 11, "A♦": 11, "A♥": 11, "A♠": 11,
+  "2♣": 2, "2♦": 2, "2♥": 2, "2♠": 2,
+  "3♣": 3, "3♦": 3, "3♥": 3, "3♠": 3,
+  "4♣": 4, "4♦": 4, "4♥": 4, "4♠": 4,
+  "5♣": 5, "5♦": 5, "5♥": 5, "5♠": 5,
+  "6♣": 6, "6♦": 6, "6♥": 6, "6♠": 6,
+  "7♣": 7, "7♦": 7, "7♥": 7, "7♠": 7,
+  "8♣": 8, "8♦": 8, "8♥": 8, "8♠": 8,
+  "9♣": 9, "9♦": 9, "9♥": 9, "9♠": 9,
+  "10♣": 10, "10♦": 10, "10♥": 10, "10♠": 10,
+  "J♣": 10, "J♦": 10, "J♥": 10, "J♠": 10,
+  "Q♣": 10, "Q♦": 10, "Q♥": 10, "Q♠": 10,
+  "K♣": 10, "K♦": 10, "K♥": 10, "K♠": 10
+}
 
 const games = {};
 io.on("connection", (socket) => {
@@ -123,14 +84,8 @@ io.on("connection", (socket) => {
         delete games[socket.id.substring(0, 4).toUpperCase()];
       });
     } else {
-      games[socket.id] = {
-        gameType: "solo",
-      };
-      socket.on("disconnect", () => {
-        delete games[socket.id];
-      });
+      window.location("single-player.html");
     }
-    console.log(games);
   });
 });
 
